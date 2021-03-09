67f6579a5c6cfd0ee294bb86e551d766ebb41745
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-03-09 18:20:07 -0500

    connection fix

--- index.js
+++ index.js
@@ -4,26 +4,23 @@ import mongoose from  'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv'
 
-import postRoustes from './routes/posts.js'
+import postRoutes from './routes/posts.js'
 
 const app = express();
 dotenv.config();
 
-app.use('/posts', postRoustes);
+
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-//Server setup
 
+app.use('/posts', postRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Hello to Metro API')
-})
-
+const CONNECTION_URL = 'mongodb+srv://Ariel:all4ara@main.mtmqh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 1347;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
     .catch((error) => console.log(error.message));
 
