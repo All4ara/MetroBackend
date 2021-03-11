606b0a47bb3be0f5eda5767cc0606e7dbe5e80ad
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-03-11 15:39:10 -0500

    refactor

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://Ariel:all4ara@main.mtmqh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
 const PORT = process.env.PORT || 1347;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
     .catch((error) => console.log(error.message));
 
