4a863f8351d4df6d479531d2c4768b7de92e360e
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-03-17 17:12:49 -0400

    back deployed

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/users', userRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Welcome to MetroBrokersNy!')
+})
+
 
 const PORT = process.env.PORT || 1347;
 
